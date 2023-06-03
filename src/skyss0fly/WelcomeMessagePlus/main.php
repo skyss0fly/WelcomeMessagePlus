d1c02c65505635a5cd2f742e0d973c08273ba440
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-03 18:27:48 +1000

    Create main.php

---
+++ src/skyss0fly/WelcomeMessagePlus/main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace skyss0fly/WelcomeMessagePlus;
+
+use skyss0fly/WelcomeMessage;
+use pocketmine/Plugin/PluginBase;
+use pocketmine/server;
+
+
+class main extends PluginBase {
+ 
+  
+  public function onLoad(): void{
+    $welcomemessage = "WelcomeMessage"
+  $welcomemessageconfig = $welcomemessage->get(Config);
+    
+
+  }
+}
